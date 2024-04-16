7a6d7011cec4ba98c44494c5de9753c4b3d28e29
NicholasBuiatti <nic.buiatti@gmail.com>
2024-04-16 15:11:15 +0200

    esercizio + bonus a caso

---
+++ JS/MyJs.js
@@ -0,0 +1,7 @@
+const nome = prompt ('inserisci il tuo nome');
+const cognome = prompt('inserisci il tuo cognome');
+const colore = prompt('inserisci il tuo colore preferito');
+
+
+// document.getElementById('persornalPassword').innerHTML = nome + cognome + colore + '#25';
+const password = document.querySelector('#persornalPassword').innerHTML = nome + cognome + colore + '#25';
---
+++ JS/MyJsGiochino.js
@@ -0,0 +1,15 @@
+alert(`Questa è la tua password: ${password}`);
+
+const quizzone = 'Quizzone Completa la canzone "Farò di te un uomo"'
+
+alert(quizzone);
+const primaParola = prompt('lo vedi quando si muovono gli alberi. Cos è?');
+const secondaParola = prompt('Sinonimo di paura.');
+const terzaParola = prompt('Cosa erutta e fa uscire lava?');
+const quartaParola = prompt('seconda persona singolare');
+
+document.getElementById('vento').innerHTML = primaParola;
+document.getElementById('timore').innerHTML = secondaParola;
+document.getElementById('vulcano').innerHTML = terzaParola;
+document.getElementById('tu').innerHTML = quartaParola;
+
